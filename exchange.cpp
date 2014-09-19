b20f68a97db301cb963750e0d0992d97f07d2e1c
Georg Walther <georgwalther@gmail.com>
2014-09-19 13:50:35 +0100

    seller can add and remove items

--- exchange.cpp
+++ exchange.cpp
@@ -2,17 +2,20 @@
 
 
 void Exchange::list( std::string item, double price ){
-  
+  items.push_back( item );
 }
 
 void Exchange::delist( std::string item ) {
-
+  items.remove( item );
 }
 
 bool Exchange::contains( std::string item ) {
+  for( auto i : items )
+    if( i == item )
+      return true;
   return false;
 }
 
 int Exchange::length( ) {
-  return this->items.size();
+  return items.size( );
 }
