b43ea5efdaae8a1cc20e661b0449535398193b91
Georg Walther <georgwalther@gmail.com>
2014-09-19 13:19:59 +0100

    rewrite functional tests as scenario story

--- functional_tests.cpp
+++ functional_tests.cpp
@@ -2,7 +2,28 @@
 #include "catch.hpp"
 #include "exchange.hpp"
 
-TEST_CASE( "Seller can list item on exchange with asking price", "[listing]" ) {
-  exchange::list("Item 1", 100);
-  REQUIRE( exchange::contains( "Item 1" ) );
+
+SCENARIO( "Sellers can add and remove offers to the exchange.", "[exchange]" ) {
+
+  GIVEN( "An empty exchange" ) {
+    Exchange exchange();
+
+    REQUIRE( exchange.length() == 0 );
+
+    WHEN( "Seller Bob adds an item" ) {
+      exchange.list( "Item 1", 100 );
+
+      THEN( "The item appears on the exchange" ) {
+        REQUIRE( exchange.contains( "Item 1" ) );
+      }
+
+      WHEN( "Seller Bob removes the item" ) {
+        exchange.delist( "Item 1" );
+
+        THEN( "The iteam does not appear on the exchange anymore" ) {
+          REQUIRE( not exchange.contains( "Item 1" ) );
+        }
+      }
+    }
+  }
 }
